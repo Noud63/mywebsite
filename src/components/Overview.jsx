5652f23f12c32bf37bed2f78a577f8f6960ac008
Noud63 <noudvandun@gmail.com>
2026-05-20 18:24:56 +0200

    fountain

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Overview = ({image}) => {
+const Overview = ({ image }) => {
   return (
     <div className="flex justify-center w-full h-auto my-4 max-xsm:px-2 relative">
-      <img
-        src={image}
-        alt="overview"
-        className=""
-      />
-         <div className="absolute flex items-center justify-center bottom-4 right-4 text-white text-md  px-4 border border-white rounded-full max-md:text-[12px] max-sm:bottom-1 max-sm:right-1">Overview</div>
+      <img src={image} alt="overview" className="" alt="noud van dun" />
+      <div className="absolute flex items-center justify-center bottom-4 right-4 text-white text-md  px-4 border border-white rounded-full max-md:text-[12px] max-sm:bottom-1 max-sm:right-1">
+        Overview
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Overview
+export default Overview;

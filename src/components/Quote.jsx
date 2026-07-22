7979a06e8ea424c9b6e20d983d1538e90059fa44
Noud63 <noudvandun@gmail.com>
2026-07-22 14:46:49 +0200

    extra content

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Quote = ({ text, name, url }) => {
   return (
-    <div className="flex flex-col justify-center items-center py-12 border-b border-t border-blue-950 my-4 max-xsm:mx-2 gap-4 max-sm:py-8">
+    <div className="flex flex-col justify-center items-center py-12 border-b border-t border-blue-950 my-4 max-xsm:mx-2 gap-4 max-sm:py-8 max-mobile:mx-2">
       <span className="text-blue-950 text-xl max-sm:text-lg text-center px-4">
         {text}
         <br />
-        <a
+        {url ? <a
           href={url}
           target="_blank"
           rel="noreferrer noopener"
@@ -15,7 +15,9 @@ const Quote = ({ text, name, url }) => {
           <span className="text-lg font-semibold max-sm:text-[15px]">
             {name}
           </span>
-        </a>
+        </a> : <span className="text-lg font-semibold max-sm:text-[15px] underline">
+            {name}
+          </span>}
       </span>
     </div>
   );

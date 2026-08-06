8f98d72982c4ffc0a864545ee166e8604ba29485
Noud63 <noudvandun@gmail.com>
2026-08-06 07:31:50 +0200

    HeroTitle restyled

--- src/components/intro/HeroTitle.jsx
+++ src/components/intro/HeroTitle.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const HeroTitle = () => {
   return (
-    <div className="flex w-full bg-gradient-to-r from-black via-slate-700 to-black text-slate-300 items-start mb-0 max-lg:mb-0 max-mobile:px-2 border-t-2 border-white">
-      <div className="w-full flex py-2 text-4xl font-medium font-serif  border-b border-blue-950 
+    <div className="flex w-full bg-gradient-to-r from-black via-slate-700 to-black text-slate-300 items-start mb-0 max-lg:mb-0 max-mobile:px-2 border-t-2 border-dotted border-slate-300">
+      <div className="w-full flex py-2 text-4xl font-medium font-serif  
       max-md:text-3xl max-sm:text-xl max-sm:font-semibold max-xxsm:text-lg">
         <div className="">/ NOUD VAN DUN </div>
         <div className="">/ PAINTINGS /</div>

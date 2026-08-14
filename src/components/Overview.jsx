da370489ff3c15653052837fe95104dc69e695bc
Noud63 <noudvandun@gmail.com>
2026-08-14 10:54:43 +0200

    Update alt field overview

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Overview = ({ image }) => {
+const Overview = ({ image, alt }) => {
   return (
     <div className="flex justify-center w-full h-auto max-mobile:px-2 relative">
-      <img src={image}  className="" alt="noud van dun" loading="lazy" />
+      <img src={image} className="" alt={alt} loading="lazy" />
       <div className="absolute flex items-center justify-center bottom-4 right-4 text-white text-md  px-4 border border-white rounded-full max-md:text-[12px] max-sm:bottom-2 max-sm:right-3">
         Overview
       </div>
